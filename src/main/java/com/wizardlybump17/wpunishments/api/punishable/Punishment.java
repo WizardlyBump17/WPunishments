e427ec63d0645a08d7452cfaf26039a967a3bdf2
WizardlyBump17 <davicraft130@gmail.com>
2020-10-17 15:31:26 -0300

    Added @Builder and duration and removed expiresIn

--- src/main/java/com/wizardlybump17/wpunishments/api/punishable/Punishment.java
+++ src/main/java/com/wizardlybump17/wpunishments/api/punishable/Punishment.java
@@ -1,34 +1,26 @@
 package com.wizardlybump17.wpunishments.api.punishable;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class Punishment {
 
     @NonNull
     private final PunishmentType type;
     @NonNull
     private final String whoPunished;
+    @NonNull
+    private final String punished;
     @Setter
-    private long expiresIn;
+    private long duration;
     private final long appliedIn;
     private final boolean permanent;
     private final String reason;
 
-    public Punishment(PunishmentType type, String whoPunished, long appliedIn) {
-        this(type, whoPunished, 0, appliedIn, true, null);
-    }
-
-    public Punishment(PunishmentType type, String whoPunished, long expiresIn, long appliedIn) {
-        this(type, whoPunished, expiresIn, appliedIn, expiresIn <= 0, null);
-    }
-
-    public Punishment(PunishmentType type, String whoPunished, long expiresIn, long appliedIn, String reason) {
-        this(type, whoPunished, expiresIn, appliedIn, expiresIn <= 0, reason);
+    public long getExpiresIn() {
+        return appliedIn + duration;
     }
 
     @AllArgsConstructor
