a8f7ecb8aad146741f84e645dbd10dd581c28828
WizardlyBump17 <davicraft130@gmail.com>
2020-10-17 15:34:02 -0300

    Added the implementation of multiple punishments support

--- src/main/java/com/wizardlybump17/wpunishments/implementation/punishable/DefaultPunishable.java
+++ src/main/java/com/wizardlybump17/wpunishments/implementation/punishable/DefaultPunishable.java
@@ -3,10 +3,13 @@ package com.wizardlybump17.wpunishments.implementation.punishable;
 import com.wizardlybump17.wpunishments.api.punishable.Punishable;
 import com.wizardlybump17.wpunishments.api.punishable.Punishment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DefaultPunishable implements Punishable {
 
     private final String name;
-    private Punishment punishment;
+    private final List<Punishment> punishments = new ArrayList<>();
 
     public DefaultPunishable(String name) {
         this.name = name;
@@ -18,12 +21,28 @@ public class DefaultPunishable implements Punishable {
     }
 
     @Override
-    public Punishment getPunishment() {
-        return punishment;
+    public List<Punishment> getPunishments() {
+        return punishments;
     }
 
     @Override
-    public void setPunishment(Punishment punishment) {
-        this.punishment = punishment;
+    public void addPunishment(Punishment punishment) {
+        for (Punishment activePunishment : getPunishments()) {
+            if (activePunishment.getType() != punishment.getType()) continue;
+            activePunishment.setDuration(activePunishment.getDuration() + punishment.getDuration());
+            return;
+        }
+        punishments.add(punishment);
+    }
+
+    @Override
+    public void removePunishment(Punishment.PunishmentType type) {
+
+    }
+
+    @Override
+    public boolean hasPunishment(Punishment.PunishmentType type) {
+        for (Punishment punishment : getPunishments()) if (punishment.getType() == type) return true;
+        return false;
     }
 }
