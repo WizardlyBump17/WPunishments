1743dffe684f1a51eba08ecdbff9b64ae168819a
WizardlyBump17 <davicraft130@gmail.com>
2020-10-22 15:18:04 -0300

    Added a util to the punishments messages

--- src/main/java/com/wizardlybump17/wpunishments/command/PunishCommand.java
+++ src/main/java/com/wizardlybump17/wpunishments/command/PunishCommand.java
@@ -17,30 +17,33 @@ public class PunishCommand extends CustomCommandExecutor {
         super(plugin, "punish");
     }
 
-    // /punish <player> <time> [type] [reason]
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length < 2) {
-            sender.sendMessage("§eUsage: §7/punish <player> <time> [type] [reason]");
+            sender.sendMessage("§eUsage: §7/punish <player> <type> [time] [reason]");
             return true;
         }
-        if (args.length == 2) {
-            if (sender instanceof Player) {
-                //chat actions
-                return true;
-            }
-            sender.sendMessage("§eUsage: §7/punish <player> <time> <type> [reason]");
-            return true;
-        }
-        String target = args[0];
-        PunishableManager punishableManager = plugin.getPunishableManager();
-        Punishable punishable = punishableManager.getOrAdd(target);
         try {
-            int time = Integer.parseInt(args[1]);
-            Punishment.PunishmentType type = Punishment.PunishmentType.valueOf(args[2].toUpperCase());
+            String target = args[0];
+            PunishableManager punishableManager = plugin.getPunishableManager();
+            Punishable punishable = punishableManager.getOrAdd(target);
+            Punishment.PunishmentType type = Punishment.PunishmentType.valueOf(args[1].toUpperCase());
             for (Punishment punishment : punishable.getPunishments())
                 if (System.currentTimeMillis() >= punishment.getExpiresIn())
                     punishable.removePunishment(punishment.getType());
+            if(args.length == 2) {
+                Punishment punishment = Punishment.builder()
+                        .type(type)
+                        .punished(target)
+                        .appliedIn(System.currentTimeMillis())
+                        .whoPunished(sender.getName())
+                        .permanent(true)
+                        .build();
+                punishable.addPunishment(punishment);
+                punishmentMessages(sender, target, punishment);
+                return true;
+            }
+            int time = Integer.parseInt(args[2]);
             String reason = args.length >= 4 ? StringUtils.join(args, ' ', 3, args.length) : null;
             Punishment punishment = Punishment.builder()
                     .type(type)
@@ -49,17 +52,10 @@ public class PunishCommand extends CustomCommandExecutor {
                     .duration(time <= 0 ? 0 : time * 1000)
                     .reason(reason)
                     .whoPunished(sender.getName())
+                    .permanent(time <= 0)
                     .build();
             punishable.addPunishment(punishment);
-            sender.sendMessage(MessageUtil.getPunishmentMessage(punishment, MessageUtil.PunishmentMessageType.WHO_PUNISHED_APPLIED));
-            Player targetPlayer = Bukkit.getPlayerExact(target);
-            if (targetPlayer == null) return true;
-            String targetMessage = MessageUtil.getPunishmentMessage(punishment, MessageUtil.PunishmentMessageType.PUNISHED_APPLIED);
-            if (punishment.getType() == Punishment.PunishmentType.MUTE) {
-                targetPlayer.sendMessage(targetMessage);
-                return true;
-            }
-            targetPlayer.kickPlayer(targetMessage);
+            punishmentMessages(sender, target, punishment);
             return true;
         } catch (NumberFormatException exception) {
             sender.sendMessage("§cInvalid punishment time!");
@@ -68,5 +64,17 @@ public class PunishCommand extends CustomCommandExecutor {
             sender.sendMessage("§cThere's no punishment type with this name (" + args[2] + ")");
             return true;
         }
+    }
+
+    private void punishmentMessages(CommandSender sender, String target, Punishment punishment) {
+        sender.sendMessage(MessageUtil.getPunishmentMessage(punishment, MessageUtil.PunishmentMessageType.WHO_PUNISHED_APPLIED));
+        Player targetPlayer = Bukkit.getPlayerExact(target);
+        if (targetPlayer == null) return;
+        String targetMessage = MessageUtil.getPunishmentMessage(punishment, MessageUtil.PunishmentMessageType.PUNISHED_APPLIED);
+        if (punishment.getType() == Punishment.PunishmentType.MUTE) {
+            targetPlayer.sendMessage(targetMessage);
+            return;
+        }
+        targetPlayer.kickPlayer(targetMessage);
     }
 }
